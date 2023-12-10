f226198b4aef8a93c4b4f056288e8adbcbf1b507
Ben Gilpin <bengilpin@icloud.com>
2023-12-09 20:56:37 -0500

    working javascript to add the default 3 cards

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,9 +1,6 @@
 // You must have an array in JavaScript with 3 default comment objects to start. Comments must have a name, a timestamp, and the comment text.
 // You must have a function that takes in one comment object as a parameter and displays it on the page using JavaScript DOM manipulation.
 
-
-
-
 // You must use an HTML Form with the following functionality:
 // That submits using the addEventListener
 // Prevents the page from reloading when submitting a new comment
@@ -33,47 +30,50 @@ const commentsArray = [
     timestamp: "12/20/2020",
     comment:
       "I can t stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can t get enough.",
-  }
+  },
 ];
 
+function createComment(commentData) {
+  const commentCard = document.createElement("div");
+  commentCard.classList.add("commentBox__containers__objects");
 
-function createComment(commentsArray) {
-    const commentCard = document.createElement("div");
-    commentCard.classList.add("commentBox__containers__objects");
+  const profilePic = document.createElement("div");
+  profilePic.classList.add("commentBox__containers--profile");
+  commentCard.appendChild(profilePic);
 
-    const profilePic = document.createElement("div");
-    profilePic.classList.add("commentBox__containers--profile");
-    commentCard.appendChild(profilePic);
+  const commentCardContent = document.createElement("div");
+  commentCardContent.classList.add("commentBox__containers__objects--content");
+  commentCard.appendChild(commentCardContent); // Fix typo here
 
-    const commentCardContent = document.createElement("div");
-    commentCardContent.classList.add("commentBox__containers__objects--content");
-    commentCard.appendChild(commendCardContent);
-  
-    const commentCardContentHeader = document.createElement("div");
-    commentCardContentHeader.classList.add("commentBox__containers__objects--header");
-    commentCardContent.appendChild(commentCardContentHeader);
+  const commentCardContentHeader = document.createElement("div");
+  commentCardContentHeader.classList.add(
+    "commentBox__containers__objects--header"
+  );
+  commentCardContent.appendChild(commentCardContentHeader);
 
-    const name = document.createElement("p");
-    name.classList.add("commentBox__containers--text--name");
-    name.classList.add("bold16");
-    name.innerText = commentsArray.name;
-    commentCardContentHeader.appendChild(name);
+  const name = document.createElement("p");
+  name.classList.add("commentBox__containers--text--name");
+  name.classList.add("bold16");
+  name.innerText = commentData.name; // Accessing properties from commentData
+  commentCardContentHeader.appendChild(name);
 
-    const date = document.createElement("p");
-    date.classList.add("commentBox__containers__objects--header--date");
-    date.innerText = commentsArray.timestamp;
-    commentCardContentHeader.appendChild(date);
+  const date = document.createElement("p");
+  date.classList.add("commentBox__containers__objects--header--date");
+  date.innerText = commentData.timestamp;
+  commentCardContentHeader.appendChild(date);
 
-    const comment = document.createElement("p");
-    comment.classList.add("commentBox__containers--text--comment");
-    comment.innerText = commentsArray.comment;
-    commentCardContent.appendChild(comment);
+  const comment = document.createElement("p");
+  comment.classList.add("commentBox__containers--text--comment");
+  comment.innerText = commentData.comment;
+  commentCardContent.appendChild(comment);
 
+  return commentCard; // Return the commentCard, not the createComment function
+}
 
+const myCommentContainer = document.querySelector(".commentsjswrapper");
 
-    
-  
-    return createComment;
-  }
+for (let i = 0; i < commentsArray.length; i++) {
+  const card = createComment(commentsArray[i]);
+  myCommentContainer.appendChild(card);
+}
 
-  createComment(commentsArray);
