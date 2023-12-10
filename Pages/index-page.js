6d9fe8e1fd36a1d6af18b033b877b3a91b087d89
Ben Gilpin <bengilpin@icloud.com>
2023-12-09 19:29:46 -0500

    I need to restructure my files now. This will probably break everything so fingers crossed.

--- Pages/index-page.js
+++ Pages/index-page.js
@@ -1,6 +1,9 @@
 // You must have an array in JavaScript with 3 default comment objects to start. Comments must have a name, a timestamp, and the comment text.
 // You must have a function that takes in one comment object as a parameter and displays it on the page using JavaScript DOM manipulation.
-// All dynamic HTML should be added to DOM via DOM Methods for individual elements. Avoid bulk assigning stringified HTML using innerHTML
+
+
+
+
 // You must use an HTML Form with the following functionality:
 // That submits using the addEventListener
 // Prevents the page from reloading when submitting a new comment
@@ -12,7 +15,7 @@
 
 //build default array
 
-let commentsArray = [
+const commentsArray = [
   {
     name: "Conor Walton",
     timestamp: "02/17/2021",
@@ -32,3 +35,45 @@ let commentsArray = [
       "I can t stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can t get enough.",
   }
 ];
+
+
+function createComment(commentsArray) {
+    const commentCard = document.createElement("div");
+    commentCard.classList.add("commentBox__containers__objects");
+
+    const profilePic = document.createElement("div");
+    profilePic.classList.add("commentBox__containers--profile");
+    commentCard.appendChild(profilePic);
+
+    const commentCardContent = document.createElement("div");
+    commentCardContent.classList.add("commentBox__containers__objects--content");
+    commentCard.appendChild(commendCardContent);
+  
+    const commentCardContentHeader = document.createElement("div");
+    commentCardContentHeader.classList.add("commentBox__containers__objects--header");
+    commentCardContent.appendChild(commentCardContentHeader);
+
+    const name = document.createElement("p");
+    name.classList.add("commentBox__containers--text--name");
+    name.classList.add("bold16");
+    name.innerText = commentsArray.name;
+    commentCardContentHeader.appendChild(name);
+
+    const date = document.createElement("p");
+    date.classList.add("commentBox__containers__objects--header--date");
+    date.innerText = commentsArray.timestamp;
+    commentCardContentHeader.appendChild(date);
+
+    const comment = document.createElement("p");
+    comment.classList.add("commentBox__containers--text--comment");
+    comment.innerText = commentsArray.comment;
+    commentCardContent.appendChild(comment);
+
+
+
+    
+  
+    return createComment;
+  }
+
+  createComment(commentsArray);
