e7700dcc49b33a8a6cd94782fdc4604e5b6b8a7e
Ben Gilpin <bengilpin@icloud.com>
2023-12-10 19:18:55 -0500

    for the 100th time, the html is FINISHED styling on mobile. Next is responsiveness for desktop and tablet with hover and active animations added to certain elements

--- scripts/build-shows.js
+++ scripts/build-shows.js
@@ -1,37 +1,35 @@
 const showsArray = [
-    {
-        date : 'Mon Sept 06 2021',
-        venue : 'Ronald Lane',
-        location : 'San Francisco, CA'
-    },
-    {
-        date : 'Tue Sept 21 2021',
-        venue : 'Pier 3 East',
-        location : 'San Francisco, CA'
-    },
-    {
-        date : 'Fri Oct 15 2021',
-        venue : 'View Lounge',
-        location : 'San Francisco, CA'
-    },
-    {
-        date : 'Sat Nov 06 2021',
-        venue : 'Hyatt Agency',
-        location : 'San Francisco, CA'
-    },
-    {
-        date : 'Fri Nov 26 2021',
-        venue : 'Moscow Center',
-        location : 'San Francisco, CA'
-    },
-    {
-        date : 'Wed Dec 15 2021',
-        venue : 'Press Club',
-        location : 'San Francisco, CA'
-    }
-]
-
-
+  {
+    date: "Mon Sept 06 2021",
+    venue: "Ronald Lane",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Tue Sept 21 2021",
+    venue: "Pier 3 East",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Fri Oct 15 2021",
+    venue: "View Lounge",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Sat Nov 06 2021",
+    venue: "Hyatt Agency",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Fri Nov 26 2021",
+    venue: "Moscow Center",
+    location: "San Francisco, CA",
+  },
+  {
+    date: "Wed Dec 15 2021",
+    venue: "Press Club",
+    location: "San Francisco, CA",
+  },
+];
 
 function createShows(shows) {
   const showContainer = document.createElement("div");
@@ -47,7 +45,7 @@ function createShows(shows) {
   miniContainer.appendChild(showDate);
 
   const showDate2 = document.createElement("p");
-  showDate2.classList.add("bold20");
+  showDate2.classList.add("upcoming-shows__containers--titles-sub");
   showDate2.innerText = shows.date;
   miniContainer.appendChild(showDate2);
   /////
@@ -61,7 +59,9 @@ function createShows(shows) {
   miniContainer2.appendChild(showVenue);
 
   const showVenue2 = document.createElement("p");
-  showVenue2.classList.add("bold20");
+  showVenue2.classList.add(
+    "upcoming-shows__containers--titles--venue-location"
+  );
   showVenue2.innerText = shows.venue;
   miniContainer2.appendChild(showVenue2);
   //////
@@ -75,7 +75,9 @@ function createShows(shows) {
   miniContainer3.appendChild(showLocation);
 
   const showLocation2 = document.createElement("p");
-  showLocation2.classList.add("bold20");
+  showLocation2.classList.add(
+    "upcoming-shows__containers--titles--venue-location"
+  );
   showLocation2.innerText = shows.location;
   miniContainer3.appendChild(showLocation2);
 
