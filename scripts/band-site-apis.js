40b67c92d7f054ae46e71b64d0a07c190fb3a2cb
Ben Gilpin <bengilpin@icloud.com>
2023-12-17 21:34:32 -0500

    everything seems to be working now

--- scripts/band-site-apis.js
+++ scripts/band-site-apis.js
@@ -1,38 +1,41 @@
-const axios = require('axios');
+const api_key = "3d55f80c-bb2b-4ddf-b49f-e9d0c361702f";
 
-class BandSiteApi {
-
-  
-
+export default class BandSiteApi {
   constructor(apiKey) {
-    this.apiKey = "f2191fdb-1d60-43d5-aa6b-670b5950e56e";
+    this.apiKey = apiKey;
     this.apiUrl = "https://project-1-api.herokuapp.com/";
   }
 
-  
+  async postComment(comment) {
+    const url = `${this.apiUrl}comments?api_key=${this.apiKey}`;
 
-  postComment(comment) {
-
-    const url = `${this.apiUrl}comments?api_key=${apiKey}`
-
-    return axios.post(url, comment)
-      .then(response => {
-
-      }
-        )
-//catch
+    try {
+      const response = await axios.post(url,comment);
+      return response;
+    } catch (error) {
+      console.error("Could not post comment", error);
+    }
   }
 
-  getComments() {
+  async getComments() {
+    const url = `${this.apiUrl}comments?api_key=${this.apiKey}`;
 
-    const url = `${this.apiUrl}comments?api_key=${apiKey}`
+    try {
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      console.error("Could not load comments", error);
+    }
+  }
 
-    return axios.get(url)
-    .then(response => {
-      const reversedComments = response.reverse();
-      return reversedComments;
+  async getShows() {
+    const url = `${this.apiUrl}shows?api_key=${this.apiKey}`;
 
-    })
-//catch
+    try {
+      const showResponse = await axios.get(url);
+      return showResponse.data;
+    } catch (error) {
+      console.error("Could not load shows", error);
+    }
   }
 }
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,105 +1,102 @@
+import BandSiteApi from "./band-site-apis.js";
 
-const commentsArray = [
-  {
-    name: "Conor Walton",
-    timestamp: "02/17/2021",
-    comment:
-      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-  },
-  {
-    name: "Emilie Beach",
-    timestamp: "01/09/2021",
-    comment:
-      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-  },
-  {
-    name: "Miles Acosta",
-    timestamp: "12/20/2020",
-    comment:
-      "I can t stop listening. Every time I hear one of their songs the vocals it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can t get enough.",
-  },
-];
+const api_key = "3d55f80c-bb2b-4ddf-b49f-e9d0c361702f";
 
-function getCurrentTimestamp() {
-  const now = new Date();
-  const month = (now.getMonth() + 1).toString().padStart(2, "0");
-  const day = now.getDate().toString().padStart(2, "0");
-  const year = now.getFullYear();
+const comments = new BandSiteApi(api_key);
 
-  return `${month}/${day}/${year}`;
-}
 
-function createComment(commentData) {
-  const commentCard = document.createElement("div");
-  commentCard.classList.add("comment-box__containers__objects");
+document.addEventListener("DOMContentLoaded", async function () {
+  // Your existing code here
 
-  const profilePic = document.createElement("div");
-  profilePic.classList.add("comment-box__containers--profile");
-  commentCard.appendChild(profilePic);
-
-  const commentCardContent = document.createElement("div");
-  commentCardContent.classList.add("comment-box__containers__objects--content");
-  commentCard.appendChild(commentCardContent); 
-
-  const commentCardContentHeader = document.createElement("div");
-  commentCardContentHeader.classList.add(
-    "comment-box__containers__objects--header"
-  );
-  commentCardContent.appendChild(commentCardContentHeader);
-
-  const name = document.createElement("p");
-  name.classList.add("comment-box__containers--text--name");
-  name.classList.add("bold16");
-  name.innerText = commentData.name; 
-  commentCardContentHeader.appendChild(name);
-
-  const date = document.createElement("p");
-  date.classList.add("comment-box__containers__objects--header--date");
-  date.innerText = commentData.timestamp;
-  commentCardContentHeader.appendChild(date);
-
-  const comment = document.createElement("p");
-  comment.classList.add("comment-box__containers--text--comment");
-  comment.innerText = commentData.comment;
-  commentCardContent.appendChild(comment);
-
-  return commentCard;
-}
-
-function renderComments() {
-  const myCommentContainer = document.querySelector(".js-wrapper-index");
-  myCommentContainer.innerHTML = "";
-
-  const reversedCommentsArray = commentsArray.slice().reverse();
-
-  for (let i = 0; i < reversedCommentsArray.length; i++) {
-    const card = createComment(reversedCommentsArray[i]);
-    myCommentContainer.appendChild(card);
+  function getCurrentTimestamp() {
+    const now = new Date();
+    const month = (now.getMonth() + 1).toString().padStart(2, "0");
+    const day = now.getDate().toString().padStart(2, "0");
+    const year = now.getFullYear();
+    console.log("timestamp working");
+    return `${month}/${day}/${year}`;
   }
-}
 
-document
-  .getElementById("comment-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+  function createComment(commentData) {
+    const commentCard = document.createElement("div");
+    commentCard.classList.add("comment-box__containers__objects");
 
-    const name = document.getElementById("name").value;
-    const comment = document.getElementById("comment").value;
+    const profilePic = document.createElement("div");
+    profilePic.classList.add("comment-box__containers--profile");
+    commentCard.appendChild(profilePic);
 
-    if (name && comment) {
-      const newComment = {
-        name: name,
-        timestamp: getCurrentTimestamp(),
-        comment: comment,
-      };
+    const commentCardContent = document.createElement("div");
+    commentCardContent.classList.add(
+      "comment-box__containers__objects--content"
+    );
+    commentCard.appendChild(commentCardContent);
 
-      commentsArray.push(newComment);
+    const commentCardContentHeader = document.createElement("div");
+    commentCardContentHeader.classList.add(
+      "comment-box__containers__objects--header"
+    );
+    commentCardContent.appendChild(commentCardContentHeader);
 
-      document.getElementById("name").value = "";
-      document.getElementById("comment").value = "";
+    const name = document.createElement("p");
+    name.classList.add("comment-box__containers--text--name");
+    name.classList.add("bold16");
+    name.innerText = commentData.name;
+    commentCardContentHeader.appendChild(name);
 
-      renderComments();
+    const date = document.createElement("p");
+    date.classList.add("comment-box__containers__objects--header--date");
+    date.innerText = getCurrentTimestamp()
+    commentCardContentHeader.appendChild(date);
+
+    const newComment = document.createElement("p");
+    newComment.classList.add("comment-box__containers--text--comment");
+    newComment.innerText = commentData.comment;
+    commentCardContent.appendChild(newComment);
+    console.log("create comment function working");
+    return commentCard;
+  }
+
+  async function renderComments() {
+    const myCommentContainer = document.querySelector(".js-wrapper-index");
+    myCommentContainer.innerHTML = "";
+
+    try {
+      const newComments = await comments.getComments();
+      const reversedComments = newComments.reverse();
+      console.log(newComments.data);
+      for (let i = 0; i < newComments.length; i++) {
+        const card = createComment(newComments[i]);
+        
+        myCommentContainer.appendChild(card);
+      }
+      console.log("comments render function working");
+    } catch (error) {
+      console.error("comment render not working");
     }
-  });
+  }
+  document
+    .getElementById("comment-form")
+    .addEventListener("submit", async function (event) {
+      event.preventDefault();
 
-renderComments();
+      const name = document.getElementById("name").value;
+      const post = document.getElementById("comment").value;
+
+      if (name && post) {
+        const newComment = {
+          name: name,
+          // timestamp: getCurrentTimestamp(),
+          comment: post
+        };
+
+        await comments.postComment(newComment);
+
+        document.getElementById("name").value = "";
+        document.getElementById("comment").value = "";
+
+        renderComments();
+      }
+    });
+
+  renderComments();
+});
